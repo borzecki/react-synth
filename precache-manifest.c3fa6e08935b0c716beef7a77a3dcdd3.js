50a3309975297561176b07905a29d8b0b5c8a49a
Daniel Borzęcki <borzecki.daniel@gmail.com>
2020-02-08 14:38:31 +0100

    Updates

---
+++ precache-manifest.c3fa6e08935b0c716beef7a77a3dcdd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45bf08c4d5400094a4773b881f485d3",
+    "url": "/react-synth/index.html"
+  },
+  {
+    "revision": "3614e99a4c6292d6299f",
+    "url": "/react-synth/static/css/main.02df27fc.chunk.css"
+  },
+  {
+    "revision": "15af4067142a044e3f92",
+    "url": "/react-synth/static/js/2.de3516d7.chunk.js"
+  },
+  {
+    "revision": "3614e99a4c6292d6299f",
+    "url": "/react-synth/static/js/main.0d4bb9a3.chunk.js"
+  },
+  {
+    "revision": "d97ce6157190f52a4339",
+    "url": "/react-synth/static/js/runtime~main.7fb3a36a.js"
+  }
+]);
